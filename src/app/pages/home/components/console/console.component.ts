afa720a3a0d1e1882faf70419e640f589f673161
Oscar <oscar@oscarprince.ch>
2023-08-09 06:34:58 +0000

    feat(console): auto scroll to bottom

--- src/app/pages/home/components/console/console.component.ts
+++ src/app/pages/home/components/console/console.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'console',
   templateUrl: './console.component.html',
   styleUrls: ['./console.component.scss']
 })
-export class ConsoleComponent {
+export class ConsoleComponent implements AfterViewChecked {
 
   public outputs: string[] = [];
   public input: string = '';
   public userDisplay = 'anonymous@oscarprince.ch:~$';
 
+  @ViewChild('console') private consoleRef!: ElementRef;
+
+  public ngAfterViewChecked(): void {
+    this.scrollToBottom();
+  }
+
   public submitInput(): void {
     this.outputs.push(`${this.userDisplay} ${this.input}`);
     this.input = '';
+    this.scrollToBottom();
+  }
+  
+  public scrollToBottom(): void {
+    this.consoleRef.nativeElement.scrollTop = this.consoleRef.nativeElement.scrollHeight + '100';
   }
 }
