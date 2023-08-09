965ea430a9facb0aab182f352c3927e493c2ea0f
Oscar <oscar@oscarprince.ch>
2023-08-09 07:08:06 +0000

    feat(console): console commands

--- src/app/pages/home/components/console/console.component.ts
+++ src/app/pages/home/components/console/console.component.ts
@@ -1,28 +1,48 @@
-import { AfterViewChecked, Component, ElementRef, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Command } from './models/command.model';
 
 @Component({
   selector: 'console',
   templateUrl: './console.component.html',
   styleUrls: ['./console.component.scss']
 })
-export class ConsoleComponent implements AfterViewChecked {
+export class ConsoleComponent implements AfterViewChecked, OnInit {
 
   public outputs: string[] = [];
   public input: string = '';
   public userDisplay = 'anonymous@oscarprince.ch:~$';
   public displayCaret = false;
+  public commands: Array<Command> = [];
 
   @ViewChild('console') private consoleRef!: ElementRef;
   @ViewChild('userInput') private userInput!: ElementRef;
+
+  constructor(
+    private http: HttpClient
+  ) {}
 
   public ngAfterViewChecked(): void {
     this.scrollToBottom();
   }
 
+  public ngOnInit(): void {
+    this.http.get<Array<Command>>('assets/commands.json')
+      .subscribe(commands => this.commands = commands);
+  }
+
   public submitInput(): void {
     this.outputs.push(`${this.userDisplay} ${this.input}`);
+    this.searchAndOutputCommand(this.input);
     this.input = '';
     this.scrollToBottom();
+  }
+
+  public searchAndOutputCommand(input: string): void {
+    const command = this.commands.find(e => e.input === input.toLowerCase());
+    if (command) {
+      this.outputs.push(command.output);
+    }
   }
   
   public scrollToBottom(): void {
---
+++ src/app/pages/home/components/console/models/command.model.ts
@@ -0,0 +1,9 @@
+export class Command {
+    input: string;
+    output: string;
+
+    constructor(input: string, output: string) {
+        this.input = input;
+        this.output = output;
+    }
+}
