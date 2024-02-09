b9df6eca1f51a8db9f21bfd39f11f32445dea60e
Penkie <oscar@oscarprince.ch>
2024-02-09 15:38:55 +0100

    feat(contact): contact form layout and style

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PagesComponent } from './pages.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: PagesComponent, children: [
@@ -11,6 +12,9 @@ const routes: Routes = [
     },
     {
       path: 'projects', component: ProjectsComponent
+    },
+    {
+      path: 'contact', component: ContactComponent
     }
   ]
   }
