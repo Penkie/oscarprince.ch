e2ad7c46f3ac04135dbf77eef63da5171e41c1e1
Penkie <oscar@oscarprince.ch>
2024-02-18 21:03:08 +0100

    refactor: remove vercel analytics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-export const  routes: Routes = [{ path: '', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) }];
+const  routes: Routes = [{ path: '', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
