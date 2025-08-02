492c6a7b03910476e561d0818e33390169a26953
Penkie <oscar@oscarprince.ch>
2025-08-02 16:18:45 +0200

    feat(projects): loading skeleton

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs';
+import { delay, map } from 'rxjs';
 import { Project } from 'src/app/common/models/project.model';
 import { ProjectsService } from 'src/app/common/services/projects.service';
 
@@ -13,6 +13,7 @@ export class ProjectsComponent implements OnInit {
 
   public projects: Project[] = [];
   public errorLoading = false;
+  public skeletonCount = Array.from({ length: 5 }, (_, i) => i);
 
   constructor(
     private projectsService: ProjectsService
@@ -21,7 +22,8 @@ export class ProjectsComponent implements OnInit {
   public ngOnInit(): void {
     this.projectsService.getProjects$()
       .pipe(
-        map(projects => projects.sort((a, b) => a.order - b.order))
+        map(projects => projects.sort((a, b) => a.order - b.order)),
+        delay(150)
       )
       .subscribe({
         next: (projects) => {
